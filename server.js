506a08e6e9fa93d88c1792edab840a350a295723
Akash Pawar <akashpawar202k@gmail.com>
2024-12-06 11:32:04 +0530

    project stagging to push to remote

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import os from 'os';
+import axios from "axios"
+
+const app = express();
+const PORT = process.env.PORT || "80"
+
+// Function to get the server's IP address
+const getServerPrivateIp = () => {
+    const networkInterfaces = os.networkInterfaces();
+    for (const iface of Object.values(networkInterfaces)) {
+        for (const address of iface) {
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address;
+            }
+        }
+    }
+    return '127.0.0.1'; // Fallback to localhost if no external IP is found
+};
+
+const serverPublicIp = async () => {
+    const response = await axios.get("https://ifconfig.me/all.json");
+    const publicIp = await response.data.ip_addr;
+    return publicIp;
+}
+// Define the root route
+app.get('/', async (req, res) => {
+    const privateIp = getServerPrivateIp();
+    const publicIp = await serverPublicIp();
+    const data = {
+        "public ip": publicIp,
+        "private ip": privateIp,
+    }
+    res.json(data)
+});
+
+// Start the server on port 80
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
