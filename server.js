473ff6edf88a7429c454d0c82d9d27e0a4915e58
Ubuntu <azureuser@akashplay.e3gzwyr3wouujeknqcr4xk5lpf.ix.internal.cloudapp.net>
2025-05-04 11:29:44 +0000

    test

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', async (req, res) => {
     const privateIp = getServerPrivateIp();
     const publicIp = await serverPublicIp();
     const data = {
-        "ping": "Pong"
+        "ping": "Pong",
         "public ip": publicIp,
         "private ip": privateIp,
     }
